974542aa552eb102afae2aab190a1705c7e0b67c
Kenneth Carter <kxccarter@gmail.com>
2020-07-10 11:30:09 -0500

    building GET route.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,16 +2,19 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
+koalaRouter.get('/', (req, res) => {
+    console.log(`In GET route!`);
+    const queryText = `SELECT * FROM koalas ORDER BY name ASC;`;
 
+    pool.query(queryText);
+});
 
 // POST
 
-
 // PUT
-
 
 // DELETE
 
