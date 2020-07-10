497cb2ac691b3ab0394d4e75f497f8836ecb5cdd
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-10 11:24:40 -0500

    created GET ajax call

---
+++ database.sql
@@ -0,0 +1,2 @@
+CREATE TABLE koalas (id SERIAL PRIMARY KEY, "name" varchar(120), gender varchar(1), age int, ready_to_transfer varchar(1), notes varchar(240));
+
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,8 +30,15 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
-} // end getKoalas
+  $.ajax({
+    type: 'GET',
+    url:'/koalas'
+  }).then(function (reponse){
+    console.log('server response:', response);
+  });
+  //append to DOM
+  // render(response);
+ // end getKoalas
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
