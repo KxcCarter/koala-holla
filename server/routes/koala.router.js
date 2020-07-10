f7c525a125739e5c0cc7c843a73e6bd389fd7bbb
Kenneth Carter <kxccarter@gmail.com>
2020-07-10 11:39:38 -0500

    Completes GET route. Tests good  in Postman.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -9,7 +9,15 @@ koalaRouter.get('/', (req, res) => {
     console.log(`In GET route!`);
     const queryText = `SELECT * FROM koalas ORDER BY name ASC;`;
 
-    pool.query(queryText);
+    pool
+        .query(queryText)
+        .then((dbResponse) => {
+            res.send(dbResponse.rows);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
 });
 
 // POST
